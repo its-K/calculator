3e849ff0652850dd55572b20cea9f0db07bb83c3
Kishore <kishore4110@gmail.com>
2021-08-12 15:04:55 +0530

    Mutiple Os support

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake
 
 class PocoTimerConan(ConanFile):
    settings = "os", "compiler", "build_type", "arch"
-   requires = "rapidjson/1.1.0","fmt/8.0.1","date/3.0.1"
+   requires = "rapidjson/1.1.0","fmt/8.0.1","boost/1.76.0"
    generators = "cmake", "gcc", "txt"
    default_options = {"poco:shared": True, "openssl:shared": True}
 
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
 #include <fmt/core.h>
+#include "boost/date_time/gregorian/gregorian.hpp"
+#include <string>
 
 using namespace rapidjson;
 
@@ -87,6 +89,14 @@ int main(int argc, char const *argv[])
     std::cout << buffer.GetString() << std::endl;
 
     fmt::print("Date\n");
+    try {
+    std::string si("2001-10-9"); //2001-October-09
+    date d(from_simple_string(si));
+    std::cout << to_simple_string(d) << std::endl;
+    }
+    catch(std::exception& e) {
+      std::cout << "  Exception: " <<  e.what() << std::endl;
+    }
 
     return 0;
 }
